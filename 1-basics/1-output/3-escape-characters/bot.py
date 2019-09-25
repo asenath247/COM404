2a1fae466e329081f4c542f7df531271415596bb
asenath247 <asenath.antwi2@gmail.com>
2019-09-25 10:07:58 +0000

    Added code

---
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -0,0 +1,6 @@
+# Display escape characters 
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Dsiplays a back slash")
+print("\" Displays a double quote")
+print("\' Dsiplays a single quote")
