8c25b5e5f777d7fe1f1f9fe570d35893dd0b9548
asenath247 <asenath.antwi2@gmail.com>
2019-09-25 10:58:10 +0000

    Added code

---
+++ 1-basics/2-input/1-user-input/bot.py
@@ -0,0 +1,9 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input("Asenath")
+print("It is nice to meet you human", name)
+
+# Read in user's name
+name = input ("What is your name human?")
+print("Nice to meet you human", name, ".")
+print("Nice to meet you " + name + ".")
