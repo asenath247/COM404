50a0b8af61d3ba21fa1f642ab82955ccb229adfe
asenath247 <asenath.antwi2@gmail.com>
2019-09-25 11:12:44 +0000

    Added code

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,15 @@
+file:///workspace/COM404/1-basics/1-output/4-ascii-art/bot.py
+
+# Please enter a character for the eye 
+o
+# Beep's expression is now as follows:
+
+##########
+#  o  o  #
+#  ----  #
+##########
+
+
+
+
+
