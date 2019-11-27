7db3f6a2654627309a45248d059fbd7a89dc63b6
Asenath Antwi <4antwa21@solent.ac.uk>
2019-11-27 12:23:26 +0000

    image frames

--- windows-layouts/4-images/2-swapping.py
+++ windows-layouts/4-images/2-swapping.py
@@ -2,11 +2,10 @@ from tkinter import *
 
 class Gui(Tk):
     
-    def __init__(self):
+def __init__(self):
         super().__init__()
 
         # load resources
-        
         self.cactusleaves_image = PhotoImage(file="U:/Problem Solving/COM404/windows-layouts/4-images/cactusleaves.gif")
         self.palmtree_image = PhotoImage(file="U:/Problem Solving/COM404/windows-layouts/4-images/palmtree.gif")
 
@@ -18,28 +17,28 @@ class Gui(Tk):
         self.__add_cactusleaves_image()
         self.__add_flip_button()
         
-    def __add_heading_label(self):
+def __add_heading_label(self):
      # create
         self.heading_label = Label()
         self.heading_label.grid(row=0, column=0, columnspan=2)
         # style
         self.heading_label.configure(text="Cactus Leaves" , font="Arial 37")
 
-    def __add_cactusleaves_image(self):
+def __add_cactusleaves_image(self):
         # create
         self.cactusleaves_label = Label()
         self.cactusleaves_label.grid(row=1, column=0)
         self.cactusleaves_label.configure(image=self.cactusleaves_image, height=480, width=480)
         
     
-    def __add_palmtree_image(self):
+def __add_palmtree_image(self):
         # create 
         self.palmtree_label = Label()
         self.palmtree_label.grid(row=1, colmun=0)
         self.palmtree_label.configure(image=self.palmtree_image, height=360, width=360)
 
 
-    def __add_flip_button(self):
+def __add_flip_button(self):
         # create
         self.flip_button = Button()
         self.flip_button.grid(row=3, column=0)
@@ -47,9 +46,9 @@ class Gui(Tk):
         self.flip_button.bind("<ButtonRelease-1>", self.__flip_button_clicked)
         self.flip_button.bind("<ButtonRelease-3>", self.__flip_button_clickedright)
 
-    def __flip_button_clicked(self, event):
+def __flip_button_clicked(self, event):
         self.cactusleaves_label.configure(image = self.palmtree_image)
-    def __flip_button_clickedright(self, event):
+def __flip_button_clickedright(self, event):
         self.cactusleaves_label.configure(image = self.cactusleaves_image)
 
 if (__name__ == "__main__"):
---
+++ windows-layouts/4-images/3-positioning.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+class Gui(Tk):
+
+    def __init__(self):
+        super().__init__()
+        
+        # load resources
+        self.map_image = PhotoImage(file="U:/Problem Solving/COM404/windows-layouts/4-images/map.gif")
+        self.bus_image = PhotoImage(file="U:/Problem Solving/COM404/windows-layouts/4-images/bus.gif")
+        # set window attributes
+        self.title("Gui")
+        
+        # add components
+        self.__add_title_label()
+        self.__add_map_image_frame()
+        self.__add_map_label()
+        self.__add_bus_label()
+
+    def __add_title_label(self):
+        # create
+        self.title_label = Label()
+        self.title_label.grid(row=0, column=0)
+        self.title_label.configure(text="Bus Journey")
+
+    def __add_bus_label(self):
+        # create
+        self.bus_label  = Label(self.map_frame)
+        self.bus_label.place(y=10, x=10)
+        self.bus_label.configure(image=self.bus_image)    
+    
+
+    def __add_map_image_frame(self):
+        # create
+        self.map_frame = Frame()
+        self.map_frame.grid(row=0, column=0)
+        self.map_frame.configure(height=500, width=500)
+
+    def __add_map_label(self):
+        # create
+        self.map_label = Label(self.map_frame)
+        self.map_label.place(x=0, y=0)
+        self.map_label.configure(image=self.map_image)
+        
+if (__name__ == "__main__"):
+    gui = Gui()
+    gui.mainloop()
